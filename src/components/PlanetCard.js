8e6973b9827cba379813dcd2ffb36b1dd3b4dc51
fortunatozm <fortunatompongo@hotmail.com>
2022-02-18 16:50:04 -0300

    5. Crie um componente chamado PlanetCard - OK

--- src/components/PlanetCard.js
+++ src/components/PlanetCard.js
@@ -1,7 +1,9 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class PlanetCard extends React.Component {
   render() {
+    const { planetName, planetImage } = this.props;
     return (
       <div data-testid="planet-card">
         <p>
@@ -12,5 +14,10 @@ class PlanetCard extends React.Component {
     );
   }
 }
+
+PlanetCard.propTypes = {
+  planetName: PropTypes.string.isRequired,
+  planetImage: PropTypes.string.isRequired,
+};
 
 export default PlanetCard;
