ac24b586bcd1c89abbea095537b793b136ba76b2
fortunatozm <fortunatompongo@hotmail.com>
2022-02-18 18:51:04 -0300

    8. Renderize o componente Title dentro do componente Missions - OK

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Title from './Title';
 
 class Missions extends React.Component {
   render() {
     return (
       <div data-testid="missions">
-        Deu Certo Fortunato
+        <Title headline="Missões" />
       </div>
     );
   }
